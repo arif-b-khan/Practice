b8493d4d3663c8ae8a666af48b62ba6df354c6d0
arif khan <arif.bannehasan@gmail.com>
2015-02-25 08:14:54 -0800

    Worked with threading today and concurrent collections

---
+++ C#Console/MyPractice/SynchronizationExm/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SynchronizationExm
+{
+    public partial class Form1 : Form
+    {
+        SynchronizationContext context;
+        public Form1()
+        {
+            context = SynchronizationContext.Current;
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                int result = await LongRunningTask().ConfigureAwait(false);
+                label1.Text = result.ToString();
+            }
+            catch (Exception ex)
+            {
+                label1.Text = ex.Message;
+            }
+        }
+
+        private async Task<int> LongRunningTask()
+        {
+            int result = 0;
+            for(int i = 0; i < int.MaxValue; i++)
+            {
+                result = i;
+                if (i > 5000)
+                {
+                    throw new OperationCanceledException("Cancelled");
+                }
+            }
+            return result;
+        }
+
+        
+    }
+}
--- C#Console/MyPractice/ThreadingExample/Program.cs
+++ C#Console/MyPractice/ThreadingExample/Program.cs
@@ -1,16 +1,19 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+
 
 namespace ThreadingExample
 {
     class Program
     {
         static EventWaitHandle autoReset = new ManualResetEvent(false);
- 
+
         static void LongRunning(object state)
         {
             string threadName = Thread.CurrentThread.ManagedThreadId.ToString();
@@ -34,12 +37,129 @@ namespace ThreadingExample
             //ThreadLocalExample();
             //ThreadPoolExample();
             //TaskRunExample();
-            TaskRunAndWaitForResult();
+            //TaskRunAndWaitForResult();
+            //AsyncAwaitExample();
+            //Thread.Sleep(10000);
+            //Console.WriteLine("Comming out of main method");
+            //EnumerableParallel();
+            //ConcurrentBlockingCollectionExample();
+            //ConcurrentBagCollectionExample();
+            CancellationOfLongRunningTask();
+        }
+
+        private static void CancellationOfLongRunningTask()
+        {
+            CancellationTokenSource cts = new CancellationTokenSource();
+            CancellationToken token = cts.Token;
+            Task t = Task.Run(() =>
+            {
+                for (int i = 0; i < int.MaxValue; i++)
+                {
+                    Console.Write(i);
+                    Console.Write(",");
+                    if (token.IsCancellationRequested)
+                    {
+                        Console.WriteLine("Cannecllation was requested");
+                        throw new OperationCanceledException("Operation Cancelled exception");
+                    }
+                }
+
+            }, token).ContinueWith((result) => {
+                result.Exception.Handle(ex =>
+                {
+                    Console.WriteLine(ex.Message);
+                    return true;
+                });
+                Console.WriteLine("Task was cancelled");
+            },TaskContinuationOptions.OnlyOnCanceled);
+            Thread.Sleep(2000);
+            cts.Cancel();
+            //try
+            //{
+            //    t.Wait();
+            //}
+            //catch (AggregateException ag)
+            //{
+            //    foreach (Exception ex in ag.InnerExceptions)
+            //    {
+            //        Console.WriteLine(ex.Message);
+            //    }
+            //}
+        }
+
+        private static void ConcurrentBagCollectionExample()
+        {
+            ConcurrentBag<int> bag = new ConcurrentBag<int>();
+            for(int i = 0; i < 10; i++)
+            {
+                bag.Add(i);
+            }
+            foreach (int result in bag)
+            {
+                if (result == 5)
+                {
+                    bag.Add(100);
+                }
+                Console.WriteLine(result);
+            }
+        }
+
+        private static void ConcurrentBlockingCollectionExample()
+        {
+            BlockingCollection<string> col = new BlockingCollection<string>();
+            Task readerTask = Task.Run(() =>
+            {
+                while (true)
+                {
+                    Console.WriteLine(col.Take());
+                }
+            });
+
+            Task writer = Task.Run(() =>
+            {
+                string content = string.Empty;
+                do
+                {
+                    content = Console.ReadLine();
+                    col.Add(content);
+                } while (!String.IsNullOrWhiteSpace(content));
+            });
+            writer.Wait();
+        }
+
+        private static void EnumerableParallel()
+        {
+            var parallelQuery = Enumerable.Range(0, 100);
+            int[] result = parallelQuery.AsParallel().Where(i => i % 2 == 0).Select(e => e).ToArray();
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine(result[i]);
+            }
+        }
+
+        private static async void AsyncAwaitExample()
+        {
+            await DownloadContent();
+            Console.WriteLine("Coming out of async await method");
+        }
+
+        private static async Task DownloadContent()
+        {
+            //using (HttpClient client = new HttpClient())
+            //{
+            //    string result = await client.GetStringAsync("http://msdn.microsoft.com");
+            //    return result;
+            //}
+            Task<int> t = Task<int>.Run(() => { LongRunning(10000); return 1000; });
+            Console.WriteLine("Waiting for Download content");
+            int result = await t;
+            Console.WriteLine(result);
         }
 
         private static void TaskRunAndWaitForResult()
         {
-          Task<List<string>> result =   Task<List<string>>.Run(() => {
+            Task<List<string>> result = Task<List<string>>.Run(() =>
+            {
                 List<string> retList = new List<string>();
                 for (int i = 0; i < 100; i++)
                 {
@@ -47,13 +167,14 @@ namespace ThreadingExample
                 }
                 return retList;
             });
-          List<string> resList = result.Result;
-          Console.WriteLine(String.Join<string>(",", resList));
+            List<string> resList = result.Result;
+            Console.WriteLine(String.Join<string>(",", resList));
         }
 
         private static void TaskRunExample()
         {
-            Task t = Task.Run(() => {
+            Task t = Task.Run(() =>
+            {
                 LongRunning(1000);
             });
             t.Wait();
