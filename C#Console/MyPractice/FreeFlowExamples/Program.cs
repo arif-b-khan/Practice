1b4cd0170f45da1b52579a3b92b083d9c1bcb4d1
arif khan <arif.bannehasan@gmail.com>
2015-03-16 01:26:32 -0700

    Added katana project sample

--- C#Console/MyPractice/FreeFlowExamples/Program.cs
+++ C#Console/MyPractice/FreeFlowExamples/Program.cs
@@ -8,6 +8,7 @@ namespace FreeFlowExamples
 {
     class Base
     {
+        public string name = "Arif Khan";
         public string Name { get; set; }
         public Base()
         {
@@ -29,7 +30,8 @@ namespace FreeFlowExamples
 
     class Derived : Base
     {
-
+        public const string FirstName = "Arif";
+        public string LastName = "khan";
         public Derived(string name)
             : base(name)
         {
@@ -44,8 +46,8 @@ namespace FreeFlowExamples
     {
         public static void Run()
         {
-            Base b = new Base();
-            b.Execute();
+            //Base b = new Base();
+            //b.Execute();
             Base b1 = new Derived("Hello");
             b1.RunExecute();
         }
@@ -92,8 +94,41 @@ namespace FreeFlowExamples
             //Console.WriteLine(b1.Name);
             //b1.Name = "Fake";
             //Console.WriteLine(b1.Name);
-            
+            //RunBaseDerivedVirtualExample.Run();
+            Task<int> task = Ret1();
+            Console.WriteLine("Main Thread waiting");
+            Console.WriteLine(task.Result);
+        }
+        static async Task Sample()
+        {
+            Task.Delay(1000);
+            Console.WriteLine("Sample");
+        }
+
+        static int LoopTask()
+        {
+            int r = 0;
+            for (int i = 0; i < int.MaxValue; i++)
+            {
+                r += i;
+            }
+            return r;
         }
         
+        static async Task<int> Ret1()
+        {
+            Console.WriteLine("Waiting for Ret2..");
+            Task task0 = Sample();
+            Task<int> task1 = Ret2();
+            Task.WhenAll(task0, task1);
+            Console.WriteLine("Result Received");
+            return task1.Result;
+        }
+
+        static async Task<int> Ret2()
+        {
+            return LoopTask();
+        }
+
     }
 }
---
+++ C#Console/MyPractice/KatanaExample/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Owin;
+using Microsoft.Owin.Hosting;
+using Owin;
+
+namespace KatanaExample
+{
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        string uri = "http://localhost:8080";
+    //        using (WebApp.Start<StartUp>(uri))
+    //        {
+    //            Console.WriteLine("Started!");
+    //            Console.WriteLine("Press any key to stop");
+    //            Console.ReadLine();
+    //        }
+    //    }
+    //}
+    [assembly: OwinStartup("TestStartup", typeof(KatanaExample.MyStartup))]
+    public class MyStartup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Use(async (env, next) =>
+            {
+                Console.WriteLine("Request : " + env.Request.Path);
+                await next();
+                Console.WriteLine("Response: " + env.Response.StatusCode);
+            });
+            
+            ConfigureWebApi(app);
+
+            app.UseHelloComponent();
+        }
+
+        public void ConfigureWebApi(IAppBuilder app)
+        {
+            HttpConfiguration httpConfig = new HttpConfiguration();
+            httpConfig.Routes.MapHttpRoute("Default", "api/{controller}/{id}", new {id = RouteParameter.Optional});
+            app.UseWebApi(httpConfig);
+        }
+    }
+
+    public static class AppBuilderHelloExtension
+    {
+        public static void UseHelloComponent(this IAppBuilder app)
+        {
+            app.Use<HelloWorldComponent>();
+        }
+    }
+
+    public class HelloWorldComponent
+    {
+        Func<IDictionary<string, object>, Task> _next;
+        public HelloWorldComponent(Func<IDictionary<string, object>, Task> next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(IDictionary<string, object> environment)
+        {
+            var stream = environment["owin.ResponseBody"] as Stream;
+            using (var strWriter = new StreamWriter(stream))
+            {
+                await strWriter.WriteAsync("Hello!!");
+            }
+        }
+    }
+}
