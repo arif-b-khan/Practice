7d7be1f5a2324facb2a79b981dee25712fc8bbce
arif khan <arif.bannehasan@gmail.com>
2015-04-16 03:30:04 -0700

    added node examlpes in practice

---
+++ NodeExamples/NodeWeb/NodeWeb/app/js/test/aboutControllerSpec.js
@@ -0,0 +1,8 @@
+﻿//'use strict';
+
+//describe('Controller: public/AboutController', function () {
+//    it('should make about menu item active.', function () {
+//        debugger;
+//        expect($rootScope.activeMenu.about == 'active');
+//    });
+//});
